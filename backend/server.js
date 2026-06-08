99dc35b767f90be35ac97342e6e6c4f50bb9d2cb
rakesh <raki7445@gmail.com>
2026-06-08 20:11:45 +0530

    DB issue solved

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import chatRoutes from "./routes/chat.js";
 import cors from "cors";
+
 const app = express();
 
 app.use(express.json());
@@ -10,23 +11,23 @@ app.use(express.json());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    credentials : true
-  }),
+    credentials: true,
+  })
 );
 
 app.use("/api", chatRoutes);
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Successfully connected to DB");
-  } catch (err) {
-    console.log(err);
-  }
-};
-
 const PORT = process.env.PORT || 6767;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Successfully connected to DB");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB Connection Error:", err);
+  });
